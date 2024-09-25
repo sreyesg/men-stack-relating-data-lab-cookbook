d14919fbcc06a1cfd2e95913e4bd37e7a60eec53
sreyesg <santiagoreyes.g@gmail.com>
2024-09-24 19:29:26 -0700

    build delete functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,9 +3,22 @@ const User = require('../models/user.js')
 const router = express.Router()
 
 // Index
-router.get('/', (req, res) => {
-    res.render('foods/index.ejs')
+router.get('/', async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        if(currentUser.pantry === null){
+            res.send('There are no items to show')
+        }else{
+            res.render('foods/index.ejs', {
+                foods: currentUser.pantry
+                })
+        }
+    } catch (error) {
+      console.log(error)
+      res.redirect('/')  
+    }
 })
+
 
 // New Form
 router.get('/new', (req, res) => {
@@ -27,4 +40,23 @@ router.post('/', async (req, res) => {
         
     }
 })
+
+// Delete
+router.delete('/:foodId', async(req, res)=>{
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.pantry.id(req.params.foodId).deleteOne()
+        currentUser.save()
+        // redirect to index
+        res.redirect(`/users/${req.session.user._id}/foods`)
+        
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+
+
+// Exports
 module.exports = router
